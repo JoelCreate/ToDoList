a5ba07b553e30db6c442356926b25c4c149e6f47
Joel Wiesner <j.wiesner88@gmail.com>
2023-01-16 12:47:36 -0800

    HTML, CSS and JS
    multiple edits and changes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log ("hello")
+
+let toDoItems = []
+const button = document.getElementById("button-el")
+const clearBtn = document.getElementById("clear-btn")
+const orderedList = document.getElementById("ol-el")
+const input = document.getElementById("input-el")
+
+
+button.addEventListener("click", function() {
+    toDoItems.push(input.value)
+    input.value = ""
+    addItem()
+})
+
+function addItem(){
+    let listItems = ""
+    for( let i = 0; i < toDoItems.length; i++) {
+        listItems += `
+            <li class="animate__animated animate__bounceIn"> 
+            ${toDoItems[i]}
+            </li>
+        `
+        orderedList.innerHTML = listItems
+
+    }
+}
+
+clearBtn.addEventListener("click", function(){
+    orderedList.innerHTML = ""
+
+})
